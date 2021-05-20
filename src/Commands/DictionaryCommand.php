1ea7f768cf6f6174e9f41c83327babf496fedb13
polion1232 <polion123@yandex.ru>
2021-05-20 12:17:00 +0400

    Dictionary command

---
+++ src/Commands/DictionaryCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Polion1232\MiscStubs\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class ServiceCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:dictionary';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new empty service';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
+    public function getStub()
+    {
+        return __DIR__ . '\\stubs\\concrete_dictionary.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Services';
+    }
+}
