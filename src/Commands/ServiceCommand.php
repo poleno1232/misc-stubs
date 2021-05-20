b6e440da87d7029c9f65af46825d2ce98703c3c2
polion1232 <polion123@yandex.ru>
2021-05-20 12:15:00 +0400

    Fix service command

--- src/Commands/ServiceCommand.php
+++ src/Commands/ServiceCommand.php
@@ -6,8 +6,40 @@ use Illuminate\Console\GeneratorCommand;
 
 class ServiceCommand extends GeneratorCommand
 {
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:service';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new empty service';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Service';
+
     public function getStub()
     {
-        return $this->resolveStubPath('/stubs/service.stub');
+        return __DIR__ . '\\stubs\\service.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Services';
     }
 }
